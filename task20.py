1eb9e69fbb282f3682c545e76e38ed92277228a1
admin <alexygulik@gmail.com>
2024-10-16 16:05:59 +0300

    task20

---
+++ task20.py
@@ -0,0 +1,26 @@
+#todo: Выведите все строки данного файла в обратном порядке.
+# Для этого считайте список всех строк при помощи метода readlines().
+
+# Содержимое файла import_this.txt
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+#
+# выходные данные:
+# Complex is better than complicated.
+# Simple is better than complex.
+# Explicit is better than implicit.
+# Beautiful is better than ugly.
+
+f = open('import_this.txt', 'w+')
+f.write(
+    "Beautiful is better than ugly.\n"
+    "Explicit is better than implicit.\n"
+    "Simple is better than complex.\n"
+    "Complex is better than complicated.\n")
+f.seek(0)
+s = f.readlines()
+f.close()
+s.reverse()
+print(''.join(s).strip('\n'))
